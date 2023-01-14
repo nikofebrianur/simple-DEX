1214eb23eb94afb5234ec6ee2e98f9c7452371f9
Niko Febrianur <nikofebrianur@gmail.com>
2023-01-14 08:09:16 +0800

    get Amount utils

---
+++ my-app/utils/getAmounts.js
@@ -0,0 +1,65 @@
+import { Contract } from "ethers";
+import {
+	EXCHANGE_CONTRACT_ABI,
+	EXCHANGE_CONTRACT_ADDRESS,
+	TOKEN_CONTRACT_ABI,
+	TOKEN_CONTRACT_ADDRESS,
+} from "../constants";
+
+export const getEtherBalance = async (provider, address, contract = false) => {
+	try {
+		if (contract) {
+			const balance = await provider.getBalance(EXCHANGE_CONTRACT_ADDRESS);
+			return balance;
+		} else {
+			const balance = await provider.getBalance(address);
+			return balance;
+		}
+	} catch (error) {
+		console.error(error);
+		return 0;
+	}
+};
+
+export const getCDTokensBalance = async (provider, address) => {
+	try {
+		const tokenContract = new Contract(
+			TOKEN_CONTRACT_ADDRESS,
+			TOKEN_CONTRACT_ABI,
+			provider
+		);
+		const balanceOfCryptoDevTokens = await tokenContract.balanceOf(address);
+		return balanceOfCryptoDevTokens;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getLPTokensBalance = async (provider, address) => {
+	try {
+		const exchangeContract = new Contract(
+			EXCHANGE_CONTRACT_ADDRESS,
+			EXCHANGE_CONTRACT_ABI,
+			provider
+		);
+		const balanceOfLPTokens = await exchangeContract.balanceOf(address);
+		return balanceOfLPTokens;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getReserveOfTokens = async (provider) => { 
+	try {
+		const exchangeContract = new Contract
+		(
+			EXCHANGE_CONTRACT_ADDRESS,
+			EXCHANGE_CONTRACT_ABI,
+			provider
+		);
+		const reserve = await exchangeContract.getReserve();
+		return reserve;
+	} catch (error) {
+		console.error(error);
+	}
+};
